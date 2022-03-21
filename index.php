317eb13f35e1cda1c4bb787c70e5b5fb2ad068d0
VikneshJadhav <viknesh7802@gmail.com>
2022-03-21 02:41:25 +0100

    completed json mission1

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
     <body>
 
     <header>
+        <!--
         <a href="index.php"><img src="./images/logo.png" height="40" width="40" alt="logo de mon site web"/></a>
+        -->
         <h1>Rythm Break</h1>
         <form action="https://gaana.com/" method="get">
                     <input type="text" name="q" placeholder="Cherchez une musique" />    
@@ -46,16 +48,39 @@
             <article>
                 <h3> Image du jour du service APOD </h3>
                 <p> </p>  
+                <!--
                 <figure>                
                     <img class='monimage' src=https://apod.nasa.gov/apod/image/2203/EquinoxSunset_Christen_1852.jpg alt="image du jour par apod nasa" width='500' height='500'/>
                     <figcaption class="center">A Picturesque Equinox Sunset : Copyright © Roland Christen</figcaption>
-                </figure>
+                </figure>-->
+
+                <?php 
+                    $json = file_get_contents('img.json');
+                    $json = json_decode($json);
+                    foreach($json as $key => $value) {
+                        if($key == "url") {
+                            $url = $value;
+                        }
+                        if($key == "title") {
+                            $title = $value;
+                        }
+                        if($key == "copyright") {
+                            $copy = $value;
+                        }
+                        //echo "<li>".$key." : ".$value."</li> \n";
+                    }
+                    echo "<figure> \n";
+                    echo "<img class='monimage' src='$url' alt='image du jour par apod nasa' width='500' height='500'/> \n";
+                    echo "<figcaption class='center'>".$title." : Copyright © ".$copy."</figcaption>";
+                    echo "</figure> \n";
+    ?>
+
             </article>
 
             <article>
                 <h3> Votre localisation approximative </h3>
                 <fieldset>
-                    <legend id="legloc">Nous avons trouvé localisation approximativement</legend>
+                    <legend id="legloc">Nous avons trouvé votre localisation approximativement</legend>
                         
                         <?php    
 
@@ -63,16 +88,21 @@
                             {
                                 $addip = getenv('REMOTE_ADDR');
                                 $loc = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$addip"));
-                                $vareg = file_get_contents("http://ipinfo.io/$addip/geo");
-                                $vareg = json_decode($vareg, true);
                                 $ville = $loc["geoplugin_city"];
                                 $pays = $loc["geoplugin_countryName"];
-                                $region = $vareg['region'];
+                                $regionName = $loc['geoplugin_regionName'];
+                                $regionCode = $loc['geoplugin_regionCode'];
+                                $region = $loc['geoplugin_region'];
 
+
+                                echo "<p style='color:white'>Vous vous trouvez aux alentours de : </p>";
                                 echo "<p style='color:white'>$ville</p>";
+                                echo "<p style='color:white'>$regionName</p>";
+                                echo "<p style='color:white'>$regionCode</p>";
                                 echo "<p style='color:white'>$region</p>";
                                 echo "<p style='color:white'>$pays</p>";                     
                             }
+
 
                             echo getLocalisation();          
                         
